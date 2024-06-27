0a5c378bbb6db85a891043a64bda0beb321f12cb
Tay Kong Yu <kongyutay@gmail.com>
2024-06-27 22:06:13 +0800

    get form data

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* 记帐本列表 */
 router.get('/account', function(req, res, next) {
-  res.send('账本列表');
+  res.render('list');
 });
 
+router.post('/account', (req, res) => {
+  //因为app.js已经给了app.use(express.json())和app.use(express.urlencoded({extended:false}))的中间件调用，所以已经直接可以用req.body
+  console.log(req.body)
+  res.send('添加记录')
+})
+
 router.get('/account/create', function(req, res, next) {
-  res.send('添加记录');
+  res.render('create');
 });
 
 module.exports = router;
