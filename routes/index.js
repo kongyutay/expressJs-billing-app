c3457ae8763e5f50f34dc0644e6bdbb06d72bcdc
kongyutay <kongyutay@gmail.com>
2024-06-27 17:55:59 +0800

    create index and record page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* 记帐本列表 */
+router.get('/account', function(req, res, next) {
+  res.send('账本列表');
+});
+
+router.get('/account/create', function(req, res, next) {
+  res.send('添加记录');
 });
 
 module.exports = router;
