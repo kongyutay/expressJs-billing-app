a695f9ef4b9f03682713b0d0b50151b12806b141
Tay Kong Yu <kongyutay@gmail.com>
2024-06-29 00:40:57 +0800

    debug insert data part

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 //导入 lowdb 让它可以记录和读取数据
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileAsync');
-const adapter = new FileSync(__dirname + '/../data/db.json');
-//获取db对象
-const db = low(adapter);
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+ 
+const adapter = new FileSync(__dirname + '/../data/db.json')
+const db = low(adapter)
 
 //导入shortid
 const shortid = require('shortid');
 
 /* 记帐本列表 */
 router.get('/account', function(req, res, next) {
-  res.render('list');
+  //先获取所有的账单信息
+  let accounts = db.get('accounts').value();
+  res.render('list', {accounts: accounts});
 });
 
 router.post('/account', (req, res) => {
@@ -24,7 +26,7 @@ router.post('/account', (req, res) => {
   let id = shortid.generate();
   //写入文件
   //使用shift往上边叠加，之后查找数据库的时候从上往下找会更快
-  db.get('accounts').shift({id:id, ...req.body}).write();
+  db.get('accounts').unshift({id:id, ...req.body}).write();
   res.render('success', {msg: ':) 添加成功', url: '/account/list'});
 })
 
