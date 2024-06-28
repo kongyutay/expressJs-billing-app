4c941cfaa455dc6f5751b642f0b34aeb736183c9
Tay Kong Yu <kongyutay@gmail.com>
2024-06-28 22:52:38 +0800

    able to record data to a json file using lowdb library

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//导入 lowdb 让它可以记录和读取数据
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileAsync');
+const adapter = new FileSync(__dirname + '/../data/db.json');
+//获取db对象
+const db = low(adapter);
+
+//导入shortid
+const shortid = require('shortid');
+
 /* 记帐本列表 */
 router.get('/account', function(req, res, next) {
   res.render('list');
@@ -8,7 +18,13 @@ router.get('/account', function(req, res, next) {
 
 router.post('/account', (req, res) => {
   //因为app.js已经给了app.use(express.json())和app.use(express.urlencoded({extended:false}))的中间件调用，所以已经直接可以用req.body
-  console.log(req.body)
+  console.log(req.body);
+
+  //generate id
+  let id = shortid.generate();
+  //写入文件
+  //使用shift往上边叠加，之后查找数据库的时候从上往下找会更快
+  db.get('accounts').shift({id:id, ...req.body}).write();
   res.send('添加记录')
 })
 
