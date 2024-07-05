4c670307c46f2e0bd11898f267ce7f207acefc04
Tay Kong Yu <kongyutay@gmail.com>
2024-07-05 20:03:11 +0800

    anti csrf attack by change to post method

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -44,7 +44,7 @@ router.post('/login', (req, res) => {
 })
 
 //退出登录
-router.get('/logout', (req, res) => {
+router.post('/logout', (req, res) => {
     //销毁session
     req.session.destroy(() => {
         res.render('success', {msg: '退出成功', url: '/login'})
