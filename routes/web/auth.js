0bd09d3ac73902dcf722648ffb9fd29cca372b47
Tay Kong Yu <kongyutay@gmail.com>
2024-07-05 18:53:20 +0800

    add register feature

--- routes/web/auth.js
+++ routes/web/auth.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const UserModel = require('../../models/UserModel')
+const md5 = require('md5')
 
 router.get('/reg', (req, res) => {
     res.render('auth/reg')
+})
+
+router.post('/reg', (req, res) => {
+    UserModel.create({...req.body, password: md5(req.body.password)}, (err, data) => {
+        if(err) {
+            res.status(500).send('注册失败')
+        }
+        res.render('success', {msg:'注册成功', url: '/login'});
+    })
 })
 
 module.exports = router;
